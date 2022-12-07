8fa7676e95f5f3d0820f835fd5a8c3aee7df1acf
Humbert Evan <evan.humbert@outlook.com>
2022-12-07 23:50:03 +0100

    update

--- root/app.js
+++ root/app.js
@@ -11,14 +11,20 @@ const generatorRight = document.getElementById("generator-right");
 const resultHtml = document.getElementById("result-html");
 const resultCss = document.getElementById("result-css");
 
+// Constants
+let letter = ".letter {font-size: 0; animation: 0.1s linear anim-letter; animation-fill-mode: forwards;}"
 
+
+// Variables
 let textList; // a list of all characteres
 let fontSizeValue; // size value (int)
 let fontSizeTypeValue; // size type (em,rem, px, cm)
 let delayNumberValue; // delay value (float / int)
 let cursorActiveValue; // cursor value (bool)
 
-generateButton.addEventListener("click", generate());
+
+
+generateButton.addEventListener("click", generate);
 
 
 
@@ -33,5 +39,40 @@ function generate() {
     } else {
         cursorActiveValue = false;
     }
+
+    // reset results
+    resetResultHtml();
+    resetResultCss();
+
+    // add .letter class
+    pushResultCss(newLetter());
+    return "Done";
 }
 
+
+function resetResultHtml(){
+    resultHtml.textContent = '';
+}
+
+function resetResultCss(){
+    resultCss.textContent = '';
+}
+
+function pushResultHtml(){
+    
+}
+
+function pushResultCss(text){
+    let newP = document.createElement("p");
+    let newContent = document.createTextNode(text);
+    newP.appendChild(newContent);
+
+    resultCss.appendChild(newP);
+}
+
+function newLetter(_fontSizeValue=fontSizeValue) {
+    letter = ".letter {font-size: "+ _fontSizeValue +"; animation: 0.1s linear anim-letter; animation-fill-mode: forwards;}"
+    return letter;
+}
+
+pushResultCss(letter);
